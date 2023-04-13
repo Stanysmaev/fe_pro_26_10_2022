2cab377416133d7871362a0772f596ef0c1c76bb
Stanysmaev <yarik231988@gmail.com>
2023-04-13 22:43:38 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+console.log(`== Now start's completing of task ==`);
 /*=================TASK=================*/
 
 /*===============Solution===============*/
+let arr2 = [1, 2, 3];
+console.log(`${arr1}`);
