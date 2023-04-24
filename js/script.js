f423363f1e08590ecc624fe8199896d1c39c26da
Stanysmaev <yarik231988@gmail.com>
2023-04-24 18:45:59 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 console.log(`== Now start's completing of task ==`);
 /*=================TASK=================*/
+// За допомогою prompt запитати ім'я користувача.
+// За допомогою alert вивести "Hello, John! How are you?"
+// Де John це те, що ввів користувач
 
 /*===============Solution===============*/
-let arr2 = [1, 2, 3];
-console.log(`${arr1}`);
+let name = prompt("Уведіть ваше ім'я:", 'Станіслав');
+console.log(`Привіт, ${name}! Як твої справи?`);
